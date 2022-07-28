34b065c5f5a8d28acb8323c25f70c84d92c04f1c
Prince kumar <73013101+prince25kumar@users.noreply.github.com>
2022-07-28 11:19:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function blackTheme() {
+    document.getElementById("container").style.backgroundColor =
+      "rgb(29, 29, 29)";
+    document.getElementById("textarea").style.backgroundColor = "rgb(29, 29, 29)";
+    document.getElementById("textarea").style.color = "white";
+    document.getElementById("textarea").style.borderColor = "white";
+  }
+  function blueTheme() {
+    document.getElementById("container").style.backgroundColor = "blue";
+    document.getElementById("textarea").style.backgroundColor = "blue";
+    document.getElementById("textarea").style.color = "white";
+    document.getElementById("textarea").style.borderColor = "white";
+  }
+  
+  function redTheme() {
+    document.getElementById("container").style.backgroundColor = "red";
+    document.getElementById("textarea").style.backgroundColor = "red";
+    document.getElementById("textarea").style.color = "black";
+    document.getElementById("textarea").style.borderColor = "black";
+  }
+  
+  function greenTheme() {
+    document.getElementById("container").style.backgroundColor = "green";
+    document.getElementById("textarea").style.backgroundColor = "green";
+    document.getElementById("textarea").style.color = "black";
+    document.getElementById("textarea").style.borderColor = "black";
+  }
